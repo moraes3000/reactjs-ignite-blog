4e6928c20bcc30eedfb9b23e6ed759eff85117bd
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-27 13:19:32 -0300

    Home List layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser } from "react-icons/fi";
+import Link from 'next/link'
 
 interface Post {
   uid?: string;
@@ -28,7 +30,21 @@ interface HomeProps {
 export default function Home() {
   // TODO
   return (
-    <Header />
+    <>
+      <Header />
+
+      <section className={styles.postContainer}>
+        <Link href='/'>
+          <a>
+            <h1>Como Utilizar hooks</h1>
+            <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+
+            <small><span><FiCalendar />15 Mar 2021</span> <FiUser />Bruno Moraes</small>
+          </a>
+        </Link>
+      </section>
+
+    </>
   )
 
 }
