a11a1d5275eb3e2764d39159400609e47499bee0
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-02 21:26:10 -0300

    Add link Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import styles from './header.module.scss'
+
 export default function Header() {
   return (
     <>
       <header className={styles.headerContainer}>
-        <img src='Logo.png' alt='logo'/>
+        <Link href='/'>
+          <a> <img src='Logo.png' alt='logo' /></a>
+        </Link>
       </header>
     </>
   )
