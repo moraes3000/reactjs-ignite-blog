012fd8896cd5b9c04f3422b93eb1ca6f78f0cd39
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-02 22:48:17 -0300

    refatorando Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { GetStaticProps } from 'next';
-import Header from '../components/Header';
 
 import { getPrismicClient, } from '../services/prismic';
 import Prismic from '@prismicio/client'
-import { RichText } from 'prismic-dom'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 import { FiCalendar, FiUser } from "react-icons/fi";
 import Link from 'next/link'
+import { useState } from 'react';
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -29,22 +31,77 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ postsItem }) {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   // TODO
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy', {
+        locale: ptBR,
+      }
+
+      )
+    }
+  })
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage != 1 && nextPage == null) {
+      return
+    }
+
+    const postsResults = await fetch(`${nextPage}`).then(response => response.json())
+    // console.log(postsResults)
+    setNextPage(postsResults.next_page)
+    setCurrentPage(postsResults.page)
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy', {
+          locale: ptBR,
+        }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author
+        }
+      }
+    })
+
+    setPosts([...posts, ...newPosts])
+  }
+
   return (
     <>
-      {postsItem.map(post => (
-        <section className={styles.postContainer} key={post.title}>
-          <Link href={`/post/${post.slugs}`}>
+    <Header />
+      {posts.map(post => (
+        <section className={styles.postContainer} key={post.uid}>
+          <Link href={`/post/${post.uid}`}>
             <a>
-              <h1>{post.title}</h1>
-              <h2>{post.excerpt}</h2>
+              <h1>{post.data.title}</h1>
+              <h2>{post.data.subtitle}</h2>
 
-              <small><span><FiCalendar />{post.updateAt}</span> <FiUser />{post.author}</small>
+              <small><span>{post.first_publication_date}<FiCalendar /></span> <FiUser />{post.data.author}</small>
             </a>
           </Link>
         </section>
       ))}
+
+
+      {nextPage && (
+        <button type='button' onClick={handleNextPage}>Carregar mais posts</button>
+      )}
+
+
     </>
   )
 
@@ -54,34 +111,32 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const postsResponse = await prismic.query([
-    Prismic.Predicates.at('document.type', 'post')
+    Prismic.Predicates.at('document.type', 'posts')
   ], {
-    fetch: ['post.title', 'post.author', 'post.content'],
-    pageSize: 2,
+    pageSize: 1,
   });
-  console.log(JSON.stringify(postsResponse, null, 2))
+  // console.log(JSON.stringify(postsResponse,null,2))
 
-  const postsItem = postsResponse.results.map(post => {
+  const posts = postsResponse.results.map(post => {
     return {
-      post: post.uid,
-      // title: post.data.title[0].text
-      title: RichText.asText(post.data.title),
-      slugs: post.slugs,
-      subtitle: RichText.asText(post.data.content),
-      author: RichText.asText(post.data.author),
-      excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? 'vazio',
-      updateAt: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
-      })
-
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
     }
   })
 
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+
   return {
     props: {
-      postsItem
+      postsPagination,
     }
   }
 };
